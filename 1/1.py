334cbfc8b755f3efacb9999dc664a79be97c5c71
Benjamin Hess <hessbe@cfredavm142.hpc.amslabs.hpecorp.net>
2024-12-02 14:28:57 -0600

    Day 1 solutions

---
+++ 1/1.py
@@ -0,0 +1,30 @@
+#!env python3
+
+import sys
+
+def main():
+
+    progName = "Day1"
+    total = 0
+    list0 = []
+    list1 = []
+
+    for line in sys.stdin:
+        line.strip()
+        fields=line.split()
+        list0.append(fields[0])
+        list1.append(fields[1])
+
+    list0.sort()
+    list1.sort()
+
+    for diff in range(len(list0)):
+        total += abs(int(list0[diff]) - int(list1[diff]))
+
+    print("Total:")
+    print(total)
+
+    return
+
+if __name__ == '__main__':
+    main()
